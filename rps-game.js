876507b80e0cb6041eeb8cbd49ea1569bb8405ca
jonion <joniondev@gmail.com>
2024-04-10 13:53:05 +0300

    feat: player pick.
    Player is promted to pick rock, paper or scissors.

--- rps-game.js
+++ rps-game.js
@@ -6,10 +6,30 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function clamp (value, min, max) {
+    return Math.max(min, Math.min(max, value))
+}  
+
 function getComputerChoice(){
      return choices[getRandomInt(3)];
+}
+
+function getPlayerChoice(){
+    let input = prompt("pick either rock (1), paper (2) or scissors (3)");
+    let inputNumber = parseInt(input);
+    
+    if(!isNaN(inputNumber)){
+        let index = clamp((inputNumber - 1), 0, 2);
+        return choices[index];
+    }else{
+        console.log("not a number 1,2 or 3!");
+        getPlayerChoice();
+    }
 }
 
 for(let i = 0; i < 5; i++){
     console.log(getComputerChoice());
 }
+
+let playerPick = getPlayerChoice();
+console.log(`player's choice: ${playerPick}`);
