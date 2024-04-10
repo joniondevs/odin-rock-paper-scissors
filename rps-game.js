eb47fb70975b46dd7c9b97e04a5c87a41924e5f2
jonion <joniondev@gmail.com>
2024-04-10 13:16:36 +0300

    feat: computer's random choice.
    Computer randomly picks either a rock, paper or scissors.

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,15 @@
-console.log("hello");
+console.log("==== Welcome to Rock, Paper and Scissors Game! ====");
+
+let choices = ["rock", "paper", "scissors"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice(){
+     return choices[getRandomInt(3)];
+}
+
+for(let i = 0; i < 5; i++){
+    console.log(getComputerChoice());
+}
