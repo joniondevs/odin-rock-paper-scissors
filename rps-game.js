7a9eb8cec8aee58090761e7de094e41fb0d38080
jonion <joniondev@gmail.com>
2024-04-18 15:40:01 +0300

    refactor: play game/round only when the player acts.

--- rps-game.js
+++ rps-game.js
@@ -27,11 +27,6 @@ function getPlayerChoice(){
     }
 }
 
-function onPlayerPick(event)
-{
-    console.log(`player picked ${event.target.id}`);
-}
-
 function compareHand(pickA, pickB)
 {
     if(pickA === pickB)
@@ -63,6 +58,24 @@ function compareHand(pickA, pickB)
     }
 }
 
+function playRound(playerChoice){
+    let playerPick = playerChoice;
+    let cpuPick = getComputerChoice();
+
+    roundLog.textContent = `cpu picks ${cpuPick} and player picks ${playerPick}`;
+
+    cpuScore += compareHand(cpuPick, playerPick);
+    playerScore += compareHand(playerPick, cpuPick);
+
+    currentRound += 1;
+    resultPara.textContent = `scores after round ${currentRound}: cpu:${cpuScore} player:${playerScore}`;
+
+    if(currentRound >= rounds || playerScore > rounds / 2 || cpuScore > rounds / 2)
+    {
+        printWinner();
+    }
+}
+
 function printWinner(playerScore, cpuScore)
 {
     if(playerScore === cpuScore)
@@ -79,31 +92,19 @@ function printWinner(playerScore, cpuScore)
 
 // ui event handlers
 const buttons = document.querySelector("#player-options");
-buttons.addEventListener("click", (event) => { console.log(event.target.id); });
+buttons.addEventListener("click", (event) => { playRound(event.target.id); });
+
+const roundLog = document.querySelector("#round-log");
+
+const resultPara = document.querySelector("#round-result");
+resultPara.textContent = "Start game by picking either rock, paper or scissors";
 
 // game
 let playerScore = 0;
 let cpuScore = 0;
-let rounds = 0;
+let rounds = 5;
+let currentRound = 0;
 
-for(let i = 0; i < rounds; i++){
-    let cpuPick = getComputerChoice();
-    let playerPick = getPlayerChoice();
 
-    console.log(`cpu picks ${cpuPick}`);
-    console.log(`player picks ${playerPick}`);
-
-    cpuScore += compareHand(cpuPick, playerPick);
-    playerScore += compareHand(playerPick, cpuPick);
-
-    console.log(`scores after round ${i+1}: cpu:${cpuScore} player:${playerScore}`);
-
-    if(playerScore > rounds / 2 || cpuScore > rounds / 2)
-    {
-        break;
-    }
-}
-
-printWinner(playerScore, cpuScore);
 
 
